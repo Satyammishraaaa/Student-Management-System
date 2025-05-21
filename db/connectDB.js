69ce3b9288a6a6c9269ceb2109871ab10853ddfa
Satyam Mishra <satyammishra@Satyams-MacBook-Air.local>
2025-05-21 17:07:58 +0530

    Initial commit

---
+++ db/connectDB.js
@@ -0,0 +1,20 @@
+// db database connectivity is done by mongoose - mongoDB
+// it gives promise function
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+const liveUrl = process.env.MONGO_URI;
+// const local_URL = 'mongodb://localhost:27017/Practice_Admission_Portal'
+
+
+const connectDB = () => {
+    return mongoose.connect(liveUrl)
+    // return mongoose.connect(local_URL)
+        .then(() => {
+            // console.log("Connect Successfully")
+            console.log("MongoDB Atlas Connect Successfully")
+        }).catch((error) => {
+            console.log(error)
+        })
+}
+module.exports = connectDB;
